c997b34a7e0ae241f3252e30cf6605054093b0cc
gonzika <hziben@gmail.com>
2023-11-28 19:34:38 +0100

    introduce preact

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'reset-css'
 import './assets/style.css'
+
 import { addLight, addPlane, createScene, } from './camera/scene.ts'
 import { createBasicDots, } from './forms/forms.ts'
 import { makeObjDraggable } from './forms/dragAndDrop.ts'
@@ -8,6 +9,7 @@ import { addGridHelper } from './camera/gridHelper.ts'
 import { createGui } from './gui.ts'
 import { createDotsConnector } from './forms/dotsConnector.ts'
 import { findPathByDijkstraAlgorithm } from './algoritms/dijkstra.ts'
+
 
 
 const init = () => {
