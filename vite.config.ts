3b33c86c444c91553273eb83217b18c813a01c4f
gonzika <hziben@gmail.com>
2023-12-01 13:16:10 +0100

    build 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [preact()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/threejs-vite/'
+  }
+
+  return config
 })
